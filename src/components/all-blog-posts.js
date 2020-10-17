1c96c0770fb34e1407717b711b04a78b499279f5
Stephen J Bradshaw <stephen.j.bradshaw@gmail.com>
2020-10-17 22:12:24 +0100

    Extract blog posts page styling

--- src/components/all-blog-posts.js
+++ src/components/all-blog-posts.js
@@ -1,56 +1,15 @@
 import React from "react"
-import { Link } from "gatsby"
-import Image from "gatsby-image"
-import styled from "styled-components"
 
-const PostsList = styled.ul`
-  @media only screen and (min-width: 700px) {
-    display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(350px, 1fr));
-    column-gap: 1rem;
-  }
-`
-
-const Post = styled.li`
-  width: 100%;
-  margin: 1rem 0rem;
-
-  background-color: ${props => props.theme.cardBackground};
-  box-shadow: 5px 5px 10px 0px rgba(0, 0, 0, 0.4);
-  border-radius: 7px;
-  transition: 0.1s;
-
-  :hover {
-    transform: scale(1.01);
-  }
-`
-
-const StyledLink = styled(Link)`
-  text-decoration: none;
-`
-
-const StyledImage = styled(Image)`
-  height: 15rem;
-  border-radius: 7px 7px 0px 0px;
-`
-
-const Section = styled.section`
-  padding: 1rem;
-  text-align: left;
-`
-
-const Title = styled.h2`
-  margin-bottom: 1rem;
-  font-size: 12pt;
-`
-
-const Description = styled.p`
-  margin-bottom: 1rem;
-`
-
-const Date = styled.small`
-  text-align: left;
-`
+import {
+  PostsList,
+  Post,
+  StyledLink,
+  StyledImage,
+  Section,
+  Title,
+  Description,
+  Date,
+} from "../styles/all-blog-posts.styled"
 
 export default function AllPosts({ posts }) {
   return (
---
+++ src/styles/all-blog-posts.styled.js
@@ -0,0 +1,52 @@
+import styled from "styled-components"
+import { Link } from "gatsby"
+import Image from "gatsby-image"
+
+export const PostsList = styled.ul`
+  @media only screen and (min-width: 700px) {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(350px, 1fr));
+    column-gap: 1rem;
+  }
+`
+
+export const Post = styled.li`
+  width: 100%;
+  margin: 1rem 0rem;
+
+  background-color: ${props => props.theme.cardBackground};
+  box-shadow: 5px 5px 10px 0px rgba(0, 0, 0, 0.4);
+  border-radius: 7px;
+  transition: 0.1s;
+
+  :hover {
+    transform: scale(1.01);
+  }
+`
+
+export const StyledLink = styled(Link)`
+  text-decoration: none;
+`
+
+export const StyledImage = styled(Image)`
+  height: 15rem;
+  border-radius: 7px 7px 0px 0px;
+`
+
+export const Section = styled.section`
+  padding: 1rem;
+  text-align: left;
+`
+
+export const Title = styled.h2`
+  margin-bottom: 1rem;
+  font-size: 12pt;
+`
+
+export const Description = styled.p`
+  margin-bottom: 1rem;
+`
+
+export const Date = styled.small`
+  text-align: left;
+`
