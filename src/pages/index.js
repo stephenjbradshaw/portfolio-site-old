e727adaf6c393cd6ff4086a0bdb037fa97f699e7
Stephen J Bradshaw <stephen.j.bradshaw@gmail.com>
2020-10-19 12:49:21 +0100

    Add initial homepage content

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,16 +2,33 @@ import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import Img from "gatsby-image"
+import { Wrapper, StyledImg } from "../styles/index.styled"
 
 export default function IndexPage({ data }) {
   console.log(data)
   return (
     <Layout>
       <SEO title="Home" keywords={[`blog`, `gatsby`, `javascript`, `react`]} />
-      <h1>Hello! I'm Stephen </h1>
-      <p>I'm a full-stack developer from Manchester.</p>
-      <Img fixed={data.file.childImageSharp.fixed} />
+      <Wrapper>
+        <h1>Hello! I'm Stephen </h1>
+        <h2>I'm a full-stack developer from Manchester</h2>
+        <p>
+          I love learning new technologies and being creative, and I recently
+          graduated from{" "}
+          <a
+            href="https://www.northcoders.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Northcoders
+          </a>{" "}
+          coding bootcamp.
+        </p>
+        <p>
+          Outside of coding, I have a passion for making experimental music and art, and I enjoy cycling-touring and kayaking.
+        </p>
+        <StyledImg fluid={data.file.childImageSharp.fluid} />
+      </Wrapper>
     </Layout>
   )
 }
@@ -20,8 +37,8 @@ export const pageQuery = graphql`
   query MyQuery {
     file(relativePath: { eq: "headshot-circle.png" }) {
       childImageSharp {
-        fixed(width: 100, height: 100) {
-          ...GatsbyImageSharpFixed
+        fluid {
+          ...GatsbyImageSharpFluid_noBase64
         }
       }
     }
