a3ef767e25d34ce346d93cbb07fdb63dad53bd04
Stephen J Bradshaw <stephen.j.bradshaw@gmail.com>
2020-10-19 11:14:57 +0100

    Add image using gatsby-image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,29 @@
 import React from "react"
+import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Img from "gatsby-image"
 
-export default function IndexPage() {
+export default function IndexPage({ data }) {
+  console.log(data)
   return (
     <Layout>
       <SEO title="Home" keywords={[`blog`, `gatsby`, `javascript`, `react`]} />
-      <h1>
-        Hello! I'm Stephen{" "}
-        <span role="img" aria-label="sparkles emoji">
-          ✨
-        </span>
-      </h1>
+      <h1>Hello! I'm Stephen </h1>
       <p>I'm a full-stack developer from Manchester.</p>
+      <Img fixed={data.file.childImageSharp.fixed} />
     </Layout>
   )
 }
+
+export const pageQuery = graphql`
+  query MyQuery {
+    file(relativePath: { eq: "headshot-circle.png" }) {
+      childImageSharp {
+        fixed(width: 100, height: 100) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+  }
+`
