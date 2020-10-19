df6e7da3658081411737b0115f2ca197f47e6833
Stephen J Bradshaw <stephen.j.bradshaw@gmail.com>
2020-10-19 12:49:47 +0100

    Tweak homepage styling

---
+++ src/styles/index.styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components"
+import Img from "gatsby-image"
+
+export const Wrapper = styled.div`
+  max-width: 35rem;
+  margin: auto;
+`
+
+export const StyledImg = styled(Img)`
+margin: auto;
+max-width: 280px;`
