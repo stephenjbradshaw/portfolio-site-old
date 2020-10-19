b615d253a5aad01db5d1b7d6bff7e9d4a453c5f7
Stephen J Bradshaw <stephen.j.bradshaw@gmail.com>
2020-10-19 22:35:41 +0100

    Adjust theme colors

--- src/styles/themes.js
+++ src/styles/themes.js
@@ -12,8 +12,8 @@ export const lightTheme = {
 export const darkTheme = {
   background: "linear-gradient(to bottom, #303030, #606060)",
   text: "#fff",
-  navBackground: "#ffcf56",
-  navText: "#303030",
+  navBackground: "#575757",
+  navText: "#ffcf56",
   themeToggle: "#191970",
   cardBackground: "#424242",
   footerBackground: "linear-gradient(to bottom, #606060, #303030)",
