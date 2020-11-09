00f5468b81f85c55cc2138b75e9b6a1dc809038f
Stephen J Bradshaw <stephen.j.bradshaw@gmail.com>
2020-11-09 12:46:10 +0000

    Change footer text

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ import React from "react"
 export default function Footer({className}) {
   return (
     <footer className={className}>
-      <p>© Built by Stephen Bradshaw, {new Date().getFullYear()}</p>
+      <p>© Built with ♥ by Stephen Bradshaw, {new Date().getFullYear()}</p>
       <p>
         Source code on{" "}
         <a href="https://github.com/stephenjbradshaw/portfolio-site">Github</a>
