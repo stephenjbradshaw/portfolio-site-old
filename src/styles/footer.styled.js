babbd4e9c102d4da795a457326c6bc950b212366
Stephen J Bradshaw <stephen.j.bradshaw@gmail.com>
2020-11-09 13:27:56 +0000

    Style footer

--- src/styles/footer.styled.js
+++ src/styles/footer.styled.js
@@ -2,12 +2,23 @@ import styled from "styled-components"
 import Footer from "../components/footer"
 
 export const StyledFooter = styled(Footer)`
-  padding: 2.5rem 0rem 1rem;
+  padding: 4rem 0rem 1rem;
   text-align: center;
 
   background: ${props => props.theme.footerBackground};
 
   p {
     margin: 0rem;
+  }
+
+  .social {
+    svg {
+      height: 1.5rem;
+      width: 1.5rem;
+    }
+
+    a {
+      margin: 0.25rem;
+    }
   }
 `
